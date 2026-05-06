ad9cdf0b823b5fc7afc3cf2046ff34d156c02403
bbasil2012 <bbasil2012@gmail.com>
2026-05-06 12:13:27 +0300

    Delete install.sh

--- install.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p alc16p-v1.0
-cd alc16p-v1.0
-curl https://github.com/bbasil2012/ALC16P/releases/download/v1.0/alc16p-v1.0.zip -L -o alc16p-v1.0.zip
-unzip alc16p-v1.0.zip
-sh firmware/flashme.sh
